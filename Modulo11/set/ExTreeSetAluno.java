45c3cdf2d27c9090214fb0bed1b7945fea38a23e
Isabella Cabral <bellacabral80@gmail.com>
2023-03-09 22:31:19 -0300

    Preimeiro commit + pastasSetEx

---
+++ Modulo11/set/ExTreeSetAluno.java
@@ -0,0 +1,33 @@
+package Modulo11.set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import Modulo11.domain.Aluno1;
+
+public class ExTreeSetAluno {
+
+        public static void main(String args[]) {
+            exemploListaSimples();
+        }
+    
+        /**
+         * Para não repetir objetos na lista de alunos é necessário que a classe
+         * Aluno implemente a classe Comparable
+         * Tem ordem natural dos objetos
+         */
+        private static void exemploListaSimples() {
+            Set<Aluno1> conjunto = new TreeSet<>();
+    
+            Aluno1 a = new Aluno1("João da Silva", "Linux básico", 0);
+            Aluno1 b = new Aluno1("Antonio Sousa", "OpenOffice", 0);
+            Aluno1 c = new Aluno1("Lúcia Ferreira", "Internet", 0);
+            Aluno1 d = new Aluno1("Antonio Sousa", "OpenOffice", 0);
+            conjunto.add(a);
+            conjunto.add(b);
+            conjunto.add(c);
+            conjunto.add(d);
+            System.out.println(conjunto);
+        }
+}
+
---
+++ Modulo11/set/TestePerfomaceSet.java
@@ -0,0 +1,54 @@
+package Modulo11.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.TreeSet;
+
+import Modulo11.domain.Aluno1;
+
+public class TestePerfomaceSet {
+    public static void main(String args[]) {
+        Random r = new Random();
+
+        HashSet<Aluno1> hashSet = new HashSet<Aluno1>();
+        TreeSet<Aluno1> treeSet = new TreeSet<Aluno1>();
+        LinkedHashSet<Aluno1> linkedSet = new LinkedHashSet<Aluno1>();
+
+        // start time
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000; i++) {
+            int x = r.nextInt(1000 - 10) + 10;
+            hashSet.add(new Aluno1("João da Silva", "Linux básico", x));
+        }
+        // end time
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("HashSet: " + duration);
+
+        // start time
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000; i++) {
+            int x = r.nextInt(1000 - 10) + 10;
+            treeSet.add(new Aluno1("João da Silva", "Linux básico", x));
+        }
+        // end time
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("TreeSet: " + duration);
+
+        // start time
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000; i++) {
+            int x = r.nextInt(1000 - 10) + 10;
+            linkedSet.add(new Aluno1("João da Silva", "Linux básico", x));
+        }
+        // end time
+        endTime = System.currentTimeMillis();
+        duration = endTime - startTime;
+        System.out.println("LinkedHashSet: " + duration);
+    }
+}
