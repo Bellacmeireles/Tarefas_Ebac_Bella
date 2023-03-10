de5527f35da74ef697a2345a96d3fad226abc3c1
Isabella Cabral <bellacabral80@gmail.com>
2023-03-09 21:46:39 -0300

    Primeiro commit + PastasEXQueue

---
+++ Modulo11/queue/ExPriorityQueue.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class ExPriorityQueue {
+    public static void main(String[] args) {
+        // Ordenação Natural
+        Queue<String> queue = new PriorityQueue<String>();
+        queue.add("Short ");
+        queue.add("Very long indeed");
+        queue.add("Medium ");
+        while (queue.size() != 0) {
+            System.out.println(queue.remove());
+        }
+    }
+}
