42265bd086728e15b293334d04b1c0d5d87e2707
Isabella Cabral <bellacabral80@gmail.com>
2023-03-20 13:54:00 -0300

    .\Modulo11\TarefaMod11\MaleAndFemale.java

--- Modulo11/TarefaMod11/MaleAndFemale.java
+++ Modulo11/TarefaMod11/MaleAndFemale.java
@@ -2,82 +2,43 @@ package Modulo11.TarefaMod11;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MaleAndFemale {
-
     public static void main(String[] args) {
-        gendersTogether();
-        separatingByGender();
-    }
+        List<String> femininos = new ArrayList<>();
+        List<String> masculinos = new ArrayList<>();
 
-    private static void gendersTogether() {
-        System.out.println("****** Group of genders together ******");
-        List<NameAndGender> group1 = new ArrayList<NameAndGender>();
-        NameAndGender a = new NameAndGender("Ana", "Female");
-        NameAndGender b = new NameAndGender("Bella", "Female");
-        NameAndGender c = new NameAndGender("Caio", "Male");
-        NameAndGender d = new NameAndGender("Samuel", "Male");
-        group1.add(a);
-        group1.add(b);
-        group1.add(c);
-        group1.add(d);
-        
-        System.out.println("Genders Together: " + group1);
-        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
 
-    }
+        System.out.println("Digite o nome da pessoa e o sexo separados por um hífen (-) ");
 
-    private static void separatingByGender() {
-        System.out.println("****** Separating by gender ******");
-        String group1 = "Ana-Female, Bella-Female, Caio-Male, Samuel-Male";
-        String[] peopleArray = group1.split(", ");
+        String entrada = scanner.nextLine();
+        String[] partes = entrada.split(", ");
 
-        for (String person : peopleArray) {
-            String[] personData = person.split("-");
-            String name = personData[0];
-            String gender = personData[1];
-            System.out.println("Name: " + name + ", " + "Gender: " + gender);
-            System.out.println("");
+        String nome = partes[0].trim();
+        String sexo = partes[1].trim();
+
+        if (sexo.equalsIgnoreCase("Feminino")) {
+            femininos.add(nome);
+        } else if (sexo.equalsIgnoreCase("Masculino")) {
+            masculinos.add(nome);
+        }
+        System.out.println(nome + sexo);
+
+        System.out.println("**** Grupo Feminino ****");
+        for (String nomeF : femininos) {
+            System.out.println(nomeF);
         }
 
+        System.out.println(" ");
+
+        System.out.println("**** Grupo Masculino ****");
+        for (String nomeM : masculinos) {
+            System.out.println(nomeM);
+        }
+
+        scanner.close();
     }
+
 }
-/**
- * 
- * String group1 = "Ana,Female; Bella,Female; Caio,Male; Samuel,Male";
-        String[] peopleArray = group1.split(";");
-
-        for (String person : peopleArray) {
-            String[] personData = person.split(",");
-            String name = personData[0];
-            String gender = personData[1];
-            System.out.println("Name:" + name);
-            System.out.println("Gender:" + gender);
-        }
-
- * List<NameAndGender> group1 = new ArrayList<NameAndGender>();
-        NameAndGender a = new NameAndGender("Ana", "Female");
-        NameAndGender b = new NameAndGender("Bella", "Female");
-        NameAndGender c = new NameAndGender("Caio", "Male");
-        NameAndGender d = new NameAndGender("Samuel", "Male");
-        group1.add(a);
-        group1.add(b);
-        group1.add(c);
-        group1.add(d);
-
-        List<NameAndGender> male = new ArrayList<NameAndGender>();
-        List<NameAndGender> female = new ArrayList<NameAndGender>();
-        for (NameAndGender ng : group1) {
-            if (ng.getGender().equals("Female")) {
-                female.add(ng);
-            } else if (ng.getGender().equals("Male")) {
-                male.add(ng);
-            }
-        }
-        
-        System.out.println("By gender Male: " + male);
-        System.out.println("");
-        System.out.println("By gender Female: " + female);
-        System.out.println("");
-    }
- */
