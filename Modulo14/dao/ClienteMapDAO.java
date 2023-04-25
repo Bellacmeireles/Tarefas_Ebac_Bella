436a4b5ad0450af557e34b85d02602e200bbc671
Isabella Cabral <bellacabral80@gmail.com>
2023-04-24 21:31:41 -0300

    .\Modulo14\dao\

---
+++ Modulo14/dao/ClienteMapDAO.java
@@ -0,0 +1,62 @@
+package Modulo14.dao;
+// IMPLEMENTAÇÃO
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import Modulo14.TarefaCadastro.Cliente;
+
+public class ClienteMapDAO implements IClienteDAO {
+
+    private Map<Long, Cliente> map;
+
+    // Construtor
+    public ClienteMapDAO() {
+        this.map = new HashMap<>();
+
+    }
+
+    // Metodos
+    @Override
+    public Boolean cadastrar(Cliente cliente) {
+        if (this.map.containsKey(cliente.getCpf())) {
+            return false;
+        }
+        this.map.put(cliente.getCpf(), cliente);
+        return null;
+    }
+
+    @Override
+    public void excluir(Long cpf) {
+        Cliente clienteCadastrado = this.map.get(cpf);
+        if (clienteCadastrado != null) {
+            this.map.remove(clienteCadastrado.getCpf(), clienteCadastrado);
+        }
+    }
+
+    @Override
+    public void alterar(Cliente cliente) {
+        Cliente clienteCadastrado = this.map.get(cliente.getCpf());
+        if (clienteCadastrado != null) {
+            cliente.setNome(cliente.getNome());
+            cliente.setTel(cliente.getTel());
+            cliente.setNumero(cliente.getNumero());
+            cliente.setEnd(cliente.getEnd());
+            cliente.setCidade(cliente.getCidade());
+            cliente.setEstado(cliente.getEstado());
+        }
+    }
+
+    @Override
+    public Cliente consultar(Long cpf) {
+        this.map.get(cpf);
+        return null;
+    }
+
+    @Override
+    public Collection<Cliente> buscarTodos() {
+        return this.map.values();
+    }
+    
+}
--- Modulo14/dao/IClienteDAO.java
+++ Modulo14/dao/IClienteDAO.java
@@ -1,5 +1,14 @@
 package Modulo14.dao;
 
+import java.util.Collection;
+
+import Modulo14.TarefaCadastro.Cliente;
+
 public interface IClienteDAO {
-    
+
+    public Boolean cadastrar(Cliente cliente);
+    public void excluir(Long cpf);
+    public void alterar (Cliente cliente);
+    public Cliente consultar(Long cpf);
+    public Collection<Cliente> buscarTodos();
 }
