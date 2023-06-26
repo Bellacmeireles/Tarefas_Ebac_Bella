a711dc8fc3a5fde9ffae6025e37f8520636b4eeb
Isabella Cabral <bellacabral80@gmail.com>
2023-06-26 15:42:46 -0300

    .\Modulo19\

---
+++ Modulo19/Tarefa/ImprimiTabela.java
@@ -0,0 +1,30 @@
+package Modulo19.Tarefa;
+
+import Modulo18.Tarefa.TabelaComAnnotation;
+
+public class ImprimiTabela {
+    public static void main(String[] args) {
+
+        /* Class<?> tabela = TabelaComAnnotation.class;
+        System.out.println(tabela); */
+                
+        TabelaComAnnotation tabComAno = new TabelaComAnnotation();
+        Class<?> tabela1 = tabComAno.getClass();
+        System.out.println(tabela1); 
+
+        System.out.println(""); 
+
+       /*  Field[] fields = tabela1.getDeclaredFields();
+        for(Field field : fields) {
+                TabelaComAnnotation annotation;
+                TabelaComAnnotation tabelaAnnotation = (TabelaComAnnotation) tabComAno;
+                System.out.println("Campo: " + field.getName());
+                    System.out.println("Valor: " + field);
+                    System.out.println("Sala: " + tabComAno);
+                    System.out.println("Número da Sala: " + tabComAno.toString());
+            }
+        } */
+    }
+}
+    
+
