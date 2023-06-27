1f169a87d94816e261fa29ae13a119172183ec4a
Isabella Cabral <bellacabral80@gmail.com>
2023-06-27 12:14:02 -0300

    .\Modulo19\Tarefa\ImprimiTabela.java

--- Modulo19/Tarefa/ImprimiTabela.java
+++ Modulo19/Tarefa/ImprimiTabela.java
@@ -1,35 +1,43 @@
 package Modulo19.Tarefa;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 
-/* import java.lang.reflect.Field;
-import Modulo18.Tarefa.TabelaAnnotation; */
+import Modulo18.Tarefa.TabelaAnnotation;
 import Modulo18.Tarefa.TabelaComAnnotation;
 
 public class ImprimiTabela {
     public static void main(String[] args) {
-    
-        /* Class<?> tabela = TabelaComAnnotation.class;
-        System.out.println(tabela); */
-                
+
         TabelaComAnnotation tabComAno = new TabelaComAnnotation();
         Class<?> tabela1 = tabComAno.getClass();
-        System.out.println(tabela1); 
+        System.out.println(tabela1);
 
-        System.out.println(""); 
+        System.out.println("");
 
-       /*  Field[] fields = tabela1.getDeclaredFields();
-        for(Field field : fields) {
-                TabelaComAnnotation annotation;
-                TabelaComAnnotation tabelaAnnotation = (TabelaComAnnotation) tabComAno;
-                System.out.println("Campo: " + field.getName());
-                    System.out.println("Valor: " + field);
-                    System.out.println("Sala: " + tabComAno);
-                    System.out.println("Número da Sala: " + tabComAno.toString());
+        Field[] fields = tabela1.getDeclaredFields();
+        for (Field field : fields) {
+            Annotation[] fieldAnnotations = field.getAnnotations();
+            for (Annotation annotation : fieldAnnotations) {
+                if (annotation instanceof TabelaAnnotation) {
+                    TabelaAnnotation tabelaAnnotation = (TabelaAnnotation) annotation;
+                    System.out.println("Complete Information: " + tabelaAnnotation);
+                    System.out.println("Value: " + tabelaAnnotation.value());
+                    for (String sala : tabelaAnnotation.sala()) {
+                        System.out.println("Sala: " + sala);
+                    }
+                    System.out.println("Número da sala: " + tabelaAnnotation.numeroDaSala());
+                    System.out.println("");
+
+                }
+
             }
-        } */
+
+        }
     }
-
 }
-
-    
-
+/**
+ * //System.out.println("Sala: " + tabelaAnnotation.sala());
+ * não funciona pois o resultado no console sai:Sala:
+ * [Ljava.lang.String;@52cc8049
+ */
