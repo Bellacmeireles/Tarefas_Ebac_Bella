ff1ef881e37f4c73b5dc1f5df05eb24fa9b378a9
Isabella Cabral <bellacabral80@gmail.com>
2023-06-30 11:53:24 -0300

    .\Modulo20\Tarefa\

---
+++ Modulo20/Tarefa/Tarefa.java
@@ -0,0 +1,20 @@
+package Modulo20.Tarefa;
+
+public class Tarefa {
+    public static void main(String[] args) {
+
+        /* Forma Normal */
+        Runnable teste1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Forma comum, sem Lambda");
+            }
+        };
+        new Thread(teste1).start();
+
+        /* Agora utilizando Lambda */
+        new Thread(() -> System.out.println("Utilazando Lambda")).start();
+
+    }
+
+}
